956eb8d6d5ffc693414282b6eb22117d6424516b
Alexander Arismendy <jaabec@gmail.com>
2023-08-12 18:33:04 -0500

    Update Projects.tsx

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -64,7 +64,7 @@ const Projects = () => {
                   <div className={styles.button_info}>             
                     <Button 
                       textButton="Ver"
-                      urlProject="https://alexgeodev.github.io/Yos-Duit/"
+                      urlProject="https://alexgeodev.github.io/yos-duit/"
                     />
                   </div>
                 </section>
